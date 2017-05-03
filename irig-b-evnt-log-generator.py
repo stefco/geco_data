0581adfa852e6a9fee2f1249e5e65585b596d822
Stefan Trklja Countryman <stefan.countryman@gmail.com>
2017-05-03 11:47:25 -0700

    add overlay plots to evnt log generator

--- irig-b-evnt-log-generator.py
+++ irig-b-evnt-log-generator.py
@@ -3,8 +3,16 @@
 # A script for generating a nominal entry for the EVNT log. Note that the text
 # below is only true in the event that everything goes right!
 
-with open('graceid.txt', 'r') as f:
-    graceid = f.read().strip()
+import sys
+
+if len(sys.argv) == 1:
+    with open('graceid.txt', 'r') as f:
+        graceid = f.read().strip()
+elif '-h' in sys.argv:
+    print('USAGE: {} GRACEID'.format(sys.argv[0]))
+    exit(1)
+else:
+    graceid = sys.argv[1]
 
 title = 'IRIG-B and DuoTone Timing Checks around {} Passed'.format(graceid)
 body = ('Independent CNS-II GPS witness clocks at each end station and '
@@ -19,7 +27,15 @@ body = ('Independent CNS-II GPS witness clocks at each end station and '
         'duotone histogram script (found at $CalSVN/aligocalibration'
         '/trunk/Common/MatlabTools/timing/commissioningFrameDuotoneStat). '
         'The delays are within the 1us bounds, as '
-        'needed.\n\n').format(graceid, graceid)
+        'needed.\n\n'
+        'Additionally, DuoTone overlay plots (as seen and described in '
+        'LIGO-T1600161) are attached. These plots show +/- 15 minutes of '
+        'DuoTone signal surrounding the event with each second worth of data '
+        'plotted on the same set of axes. The plots are visually '
+        'indistinguishable from a single second worth of data, indicating that '
+        'there were no large deviations from periodicity in the DuoTone '
+        'signal.\n\n'
+        'In conclusion, timing looks good.\n').format(graceid, graceid)
 
 with open('event-log.txt', 'w') as f:
     f.write(title)
