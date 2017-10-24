d75f8dbb87d57d3e0f5a46c30ebbcc442bb75a6c
Stefan Countryman <stefan.countryman@gmail.com>
2017-10-24 15:35:17 -0400

    add ligo-proxy-init hack

--- slurm-jobs/ligo-frame-download.sh
+++ slurm-jobs/ligo-frame-download.sh
@@ -5,8 +5,12 @@
 #SBATCH --account=geco
 #SBATCH --job-name=LIGOFrameDownload
 #SBATCH -c 1                 # number of CPU cores to use
-#SBATCH --time=2:00:00:00    # two days
+#SBATCH --time=24:00:00    # run for a day
 #SBATCH --mem-per-cpu=2gb
+
+# not a great hack
+pass=$(cat /rigel/home/stc2117/ligopass.txt)
+/rigel/home/stc2117/bin/hacked-ligo-proxy-init stefan.countryman:${pass}
 
 cd /rigel/home/stc2117/frames
 get_whole_frame_files.py -t 1186704000 -d 86400
