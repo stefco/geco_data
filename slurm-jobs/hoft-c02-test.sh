3dcbfff660f36a9cf8e35f4e5b5894df307c0a7e
Stefan Countryman <stc2117@columbia.edu>
2018-01-22 17:21:59 -0500

    add 4096 second test of C02 frame download functionality.

---
+++ slurm-jobs/hoft-c02-test.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+#
+# Download one day worth of LIGO data
+#
+#SBATCH --account=geco
+#SBATCH --job-name=LIGOFrameDownloadBNS
+#SBATCH -c 1                 # number of CPU cores to use
+#SBATCH --time=72:00:00      # run for 3 days
+#SBATCH --mem-per-cpu=2gb
+
+# BNS merger date
+start=1187022080
+
+# output directory
+outdir=/rigel/geco/users/stc2117/test-hoft-fix-2
+
+# how many seconds of data in each frame?
+frame_length=4096
+
+# how many seconds of data to fetch?
+deltat=4096
+
+# not a great hack
+pass="$(cat /rigel/home/stc2117/ligopass.txt)"
+/rigel/home/stc2117/bin/hacked-ligo-proxy-init "stefan.countryman:${pass}"
+
+get_whole_frame_files.py \
+    --start                 "${start}" \
+    --deltat                "${deltat}" \
+    --outdir                "${outdir}" \
+    --length                "${frame_length}" \
+    --hanford-frametypes    H1_HOFT_C02 \
+    --livingston-frametypes L1_HOFT_C02
+
+# mark end time
+printf 'JOB EXITING.\n'
+date
