b751586d4f5f6c9bd7a03285cb6e822f8f760e5b
Stefan Countryman <stc2117@columbia.edu>
2018-01-22 20:45:35 -0500

    add last modified time to corrupt frame finder

--- find_corrupt_frame_files.sh
+++ find_corrupt_frame_files.sh
@@ -15,7 +15,8 @@ if [ "$1"z = -hz ]; then
 fi
 
 for f in *.gwf; do
-    if ! cmp .${f}.{remote,local}_sha256.txt 2>/dev/null; then
-        echo corrupt file: $f
+    if ! cmp ."${f}".{remote,local}_sha256.txt 2>/dev/null; then
+        dt="$(( $(date +%s) - $(stat -c %Y "${f}") ))"
+        printf 'corrupt file, last mod t-%s:\t%s\n' "${dt}" "${f}"
     fi
 done
