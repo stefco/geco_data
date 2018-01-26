40de6485e794e908846525695ec4576757ff6493
Stefan Countryman <stc2117@columbia.edu>
2018-01-26 14:08:01 -0500

    also list removed outliers in download speed plot

--- show_download_speed.sh
+++ show_download_speed.sh
@@ -29,12 +29,19 @@ mtimes = numpy.loadtxt(sys.stdin)
 dtimes = mtimes[1:] - mtimes[:-1]
 
 # remove outliers
-dtimes = dtimes[numpy.argwhere(dtimes < dtimes.mean() + ${SIGMAX}*dtimes.std())]
+non_outlier_inds = numpy.argwhere(dtimes < dtimes.mean() + ${SIGMAX}*dtimes.std())
+outlier_inds = numpy.argwhere(dtimes >= dtimes.mean() + ${SIGMAX}*dtimes.std())
+clean_dtimes = dtimes[non_outlier_inds]
+
+# list outliers
+msg = ''
+for i in outlier_inds:
+    msg += str(dtimes[i]) + ', '
 
 # make a histogram
 fig = plt.figure()
-n, bins, patches = plt.hist(dtimes, 30)
-plt.text(0.6,0.6,'Outliers > \$${SIGMAX}\sigma\$\nremoved', transform=fig.transFigure)
+n, bins, patches = plt.hist(clean_dtimes, 30)
+plt.text(0.6,0.6,'Outliers > \$${SIGMAX}\sigma\$\nremoved:\n'+msg, transform=fig.transFigure)
 plt.xlabel('Delay between successive file downloads [\$s\$]')
 plt.ylabel('Count')
 plt.title('Download speeds for GWF files in\n$(pwd)')
